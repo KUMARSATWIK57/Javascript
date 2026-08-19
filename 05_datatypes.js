b9d6df0e4a2c1dcbcc510cad4e5c766a666e3000
KUMARSATWIK57 <kumarsatwik57@gmail.com>
2026-08-19 23:42:03 +0530

    05_dataypes

---
+++ 05_datatypes.js
@@ -0,0 +1,36 @@
+// Primitive 
+
+// 7 types : String , Number , Boolean, null, undeined, Symbol, BigInt 
+
+const score =100
+const scorevalue =100.3
+
+const isLoggedIn =false
+const outsideTemp =null
+let userEmail;
+
+const id =Symbol('123')
+const anotherId =Symbol('123')
+
+console.log(id === anotherId);
+
+
+// const bigNumber = 761827469237093734n
+
+
+// Reference (Non-Primitive )
+
+// array, Objects, Functions
+
+const heroes = ["shaktiman", "naagraj", "doga"]
+ let myObj = {
+    name : "hitesh",
+    age : 20,
+}
+
+const myfunction = function(){
+    console.log('HElloWorld');
+    
+}
+console.log(typeof myfunction);
+
--- test.js
+++ test.js
@@ -1,2 +1,1 @@
 console.log("Satwik")
-console.log("Rahul")
