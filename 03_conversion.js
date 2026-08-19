f528075b806acd5b9852f2d77764bd8d8d65fdb1
KUMARSATWIK57 <kumarsatwik57@gmail.com>
2026-08-19 19:18:22 +0530

    03_opeartion

--- 03_conversion.js
+++ 03_conversion.js
@@ -26,3 +26,33 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log( typeof stringNumber);
 
+// ************************* Operations *********************
+
+let value =3
+let neavalue = -value
+console.log(neavalue);
+
+let str1 ="hello"
+let str2 =" hitesh"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(2 + 2 + "1");
+
+console.log(+true);
+console.log(+"");
+
+
+let counter=100
+counter++
+console.log(counter);
+
+
+
+
+
+
+
